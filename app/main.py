6de724b12d949bc6432c91b123f64373c5dda497
Chandra Lama <iamme24cl@gmail.com>
2024-05-02 13:52:52 -0700

    added endpoints

---
+++ app/main.py
@@ -0,0 +1,34 @@
+from typing import List, Optional
+from fastapi import FastAPI, Query
+from resolver import random_items, random_genres_items
+from recommender import item_based_recommendation, user_based_recommendation
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
+
+@app.get("/all")
+async def all_movies():
+    result = random_items()
+    return {"result": result}
+
+@app.get("/genres/{genre}")
+async def genre_movies(genre: str):
+    result = random_genres_items(genre)
+    return {"result": result}
+
+@app.get("/user-based")
+async def user_based(params: Optional[List[str]] = Query(None)):
+    input_ratings_dict = dict(
+        (int(x.split(":")[0]), float(x.split(":")[1])) for x in params
+    )
+    result = user_based_recommendation(input_ratings_dict)
+    return {"result": result}
+
+@app.get("/item-based/{item_id}")
+async def item_based(item_id: str):
+    result = item_based_recommendation(item_id)
+    return {"result": result}
+
---
+++ app/movie_preprocessor.py
@@ -0,0 +1,52 @@
+import time
+import pandas as pd
+import requests
+from tqdm import tqdm
+
+API_KEY = "1a6c4e5e32ef52db99dbfba4d9f12e4a" 
+
+def add_url(row):
+    return f"http://www.imdb.com/title/tt{row}/"
+
+def add_rating(df):
+    ratings_df = pd.read_csv("data/ratings.csv")
+    ratings_df["movieId"] = ratings_df["movieId"].astype(str)
+    agg_df = ratings_df.groupby("movieId").agg(
+        rating_count=("rating","count"),
+        rating_avg=("rating","mean")
+    ).reset_index()
+    ratings_added_df = df.merge(agg_df, on="movieId")
+    return ratings_added_df
+
+def add_poster(df):
+    for i, row in tqdm(df.iterrows(), total=df.shape[0]):
+        tmdb_id = row["tmdbId"]
+        tmdb_url = f"https://api.themoviedb.org/3/movie/{tmdb_id}?api_key={API_KEY}"
+        result = requests.get(tmdb_url)
+        try:
+            df.at[i, "poster_path"]="https://image.tmdb.org/t/p/original"+result.json()["poster_path"]
+            time.sleep(0.1) # to make 0.1s time interval
+        except(TypeError, KeyError) as e:
+            # toy story poster as default
+            df.at[i, "poster_path"]="https://image.tmdb.org/t/p/original/uXDfjJbdP4ijW5hWSBrPrlKpxab.jpg"
+    return df
+    
+if __name__ == "__main__":
+    movies_df = pd.read_csv("data/movies.csv")
+    movies_df["movieId"] = movies_df["movieId"].astype(str)
+    links_df = pd.read_csv("data/links.csv", dtype=str)
+    merged_df = movies_df.merge(links_df, on="movieId", how="left")
+    merged_df["url"] = merged_df["imdbId"].apply(lambda x: add_url(x))
+    result_df = add_rating(merged_df)
+    result_df["poster_path"] = None
+    result_df = add_poster(result_df)
+    result_df.to_csv("data/movies_final.csv", index=None)
+    
+    
+    
+    
+    
+    print(result_df)
+    
+    
+    
